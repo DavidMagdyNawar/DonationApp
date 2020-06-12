4fe7cf75dfd69646c846220aa136f130560b84c8
DavidRiad <davidmagdynawar@gmail.com>
2020-06-12 16:07:05 +0200

    setting up firebase database

--- app/src/main/java/com/grad/graduationproject/MainActivity.kt
+++ app/src/main/java/com/grad/graduationproject/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.grad.graduationproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var layoutManager: LinearLayoutManager
     var categotyList: MutableList<Category> = mutableListOf()
+    val database = FirebaseDatabase.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
+
         categotyList
         layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
         rv_category.layoutManager = layoutManager
